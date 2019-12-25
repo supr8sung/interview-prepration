baeeae2ac0d915671edf708636b1cdd57990027d
Supreet Singh <supreetsingh106@gmail.com>
2019-12-26 01:12:40 +0530

    add travis integration

--- src/main/java/geek/ds/arrays/rotation/RotateArray.java
+++ src/main/java/geek/ds/arrays/rotation/RotateArray.java
@@ -1,4 +1,31 @@
 package geek.ds.arrays.rotation;
 
 public class RotateArray {
+
+    // Program for array rotation
+    // Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+
+    static int[] rotateLeft(int[] arr) {
+        if (arr.length == 0)
+            throw new IllegalArgumentException("Array size can not be empty");
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = temp;
+        return arr;
+    }
+
+    static int[] rotateRight(int[] arr) {
+        if (arr.length == 0)
+            throw new IllegalArgumentException("Array size can not be empty");
+        int temp = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = temp;
+        return arr;
+    }
+
+
 }
--- src/test/java/geek/ds/arrays/rotation/RotateArrayTest.java
+++ src/test/java/geek/ds/arrays/rotation/RotateArrayTest.java
@@ -1,7 +1,35 @@
 package geek.ds.arrays.rotation;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static geek.ds.arrays.rotation.RotateArray.rotateLeft;
+import static geek.ds.arrays.rotation.RotateArray.rotateRight;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class RotateArrayTest {
 
+    @Test
+    public void should_be_able_to_left_rotate_an_array() {
+        int[] input = {1, 2, 3, 4, 5};
+        int[] output = {2, 3, 4, 5, 1};
+        assertThat(rotateLeft(input)).isEqualTo(output);
+    }
+
+    @Test
+    public void shoulde_be_able_to_return_same_array_if_only_single_element_is_present() {
+        int[] input = {1};
+        int[] output = {1};
+        assertThat(rotateLeft(input)).isEqualTo(output);
+
+    }
+
+    @Test
+    public void should_be_able_to_right_rotate_an_array() {
+
+        int[] input = {1, 2, 3, 4, 5};
+        int[] output = {5, 1, 2, 3, 4};
+        assertThat(rotateRight(input)).isEqualTo(output);
+
+    }
 }
